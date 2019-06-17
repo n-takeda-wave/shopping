c593ed42e28665b9f09dfff1e51e2e5b0b2f0312
nami.takeda <lose.lily.nami@gmail.com>
2019-06-17 09:09:41 +0000

    product_create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,22 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+  
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to products_url
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:image, :title, :capture, :price)
+  end
 end
