9d7d6f59ae7895af61df3ecc9446b86d77b401c9
nami.takeda <lose.lily.nami@gmail.com>
2019-06-27 13:53:56 +0000

    product-store-relationships

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,13 @@
 class ProductsController < ApplicationController
+  before_action :require_store_logged_in, only: [:new, :create, :edit, :destroy]
+  before_action :correct_store, only: [:destroy]
+  
   def index
     @products = Product.search(params[:search]).page(params[:page])
   end
   
   def show
-    @product = Product.find_by(params[:id])
+    @product = Product.find(params[:id])
   end
   
   def new
@@ -12,13 +15,13 @@ class ProductsController < ApplicationController
   end
   
   def create
-    @product = Product.new(product_params)
+    @product = current_store.products.build(product_params)
 
     if @product.save
-      flash[:success] = 'ユーザを登録しました。'
+      flash[:success] = '商品を登録しました。'
       redirect_to products_url
     else
-      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      flash.now[:danger] = '商品の登録に失敗しました。'
       render :new
     end
   end
@@ -40,16 +43,21 @@ class ProductsController < ApplicationController
   end
   
   def destroy
-    @product = Product.find(params[:id])
     @product.destroy
-    
-    flash[:success] = "正常に削除されました"
-    redirect_to products_url
+    flash[:success] = 'メッセージを削除しました。'
+    redirect_back(fallback_location: root_path)
   end
   
   private
 
   def product_params
     params.require(:product).permit(:image, :title, :capture, :price)
+  end
+  
+  def correct_store
+    @product = current_store.products.find_by(id: params[:id])
+    unless @product
+      redirect_to login_url
+    end
   end
 end
